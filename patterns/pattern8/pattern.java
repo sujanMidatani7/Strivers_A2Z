16f461e0016e054136aeb4d13cb9ec5444b77527
Sujan midatani <sujanmidatani7@gmail.com>
2024-05-09 01:41:45 +0530

    Create pattern.java

---
+++ patterns/pattern8/pattern.java
@@ -0,0 +1,11 @@
+import java.math.*;
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=n;i>0;i--){
+        for(int j=0;j<n-i;j++)System.out.print(" ");
+        for(int j=0;j<2*i-1;j++)System.out.print("*");
+        System.out.println();
+        }
+    }
+}
