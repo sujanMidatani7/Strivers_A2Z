3aea7e31cd25fc8d97394b0002d86710f3e4a24d
Sujan midatani <sujanmidatani7@gmail.com>
2024-05-09 01:10:38 +0530

    Create pattern.java

---
+++ patterns/pattern3/pattern.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++)System.out.printf("%d %s",j+1," ");
+        System.out.println();
+        }
+    }
+}
